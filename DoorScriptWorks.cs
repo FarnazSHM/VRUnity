1d33ac96458bdff3b511ea8bf96724e8ca12e1d5
FarnazSHM <52172144+FarnazSHM@users.noreply.github.com>
2019-06-24 18:47:34 +0200

    Door open and close using  keys

---
+++ DoorScriptWorks.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript1 : MonoBehaviour
+{
+
+    public float speed;
+    public float angle;
+    public Vector3 direction;
+
+    // Use this for initialization
+    void Start()
+    {
+        angle = transform.eulerAngles.y;
+    }
+
+    void Update()
+    {
+       if (Mathf.Round(transform.eulerAngles.y) != angle)
+        { 
+         transform.Rotate(direction * speed);
+       }
+        if (Input.GetKeyDown(KeyCode.C))   // VR Controller key for close 
+        {
+            angle = 90;
+            direction = Vector3.up;
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))  // VR Controller key for close 
+        {
+            angle = 0;
+            direction = -Vector3.up;
+        }
+
+    }
+
+}
